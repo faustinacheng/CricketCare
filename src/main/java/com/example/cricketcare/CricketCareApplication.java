11edfe3109ddc8c36809f4441c39f72c9f0879d9
Faustina <83921854+faustinacheng@users.noreply.github.com>
2023-11-29 00:12:14 -0500

    Initial setup of web app (made by all)

---
+++ src/main/java/com/example/cricketcare/CricketCareApplication.java
@@ -0,0 +1,13 @@
+package com.example.cricketcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketCareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CricketCareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cricketcare/CricketCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cricketcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricketCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
