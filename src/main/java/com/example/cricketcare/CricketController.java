b0984c03b4c667c54e5687c62a126c738ab579ab
Faustina <83921854+faustinacheng@users.noreply.github.com>
2023-11-30 02:55:59 -0500

    First createclient API call setup

--- src/main/java/com/example/cricketcare/CricketController.java
+++ src/main/java/com/example/cricketcare/CricketController.java
@@ -1,5 +1,8 @@
 package com.example.cricketcare;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,14 +10,28 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+//import org.apache.http.HttpEntity;
+//import org.apache.http.HttpResponse;
+//import org.apache.http.client.HttpClient;
+//import org.apache.http.client.methods.HttpPost;
+//import org.apache.http.entity.ContentType;
+//import org.apache.http.impl.client.HttpClients;
+//import org.apache.http.util.EntityUtils;
+
+
 @Controller
 public class CricketController {
+    String service = "http://localhost:8081/api/v1/reservation/";
+    private final RestTemplate restTemplate;
+    Long clientId = -1L;
 
-//        @GetMapping("/")
-//        public String root(Model model) {
-//            model.addAttribute("message", "Cricket Care");
-//            return "welcome";
-//        }
+        @Autowired
+        public CricketController(RestTemplate restTemplate) {
+            this.restTemplate = restTemplate;
+        }
 
         @GetMapping("/greeting")
         public String cricket(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
@@ -29,9 +46,18 @@ public class CricketController {
         }
 
         @PostMapping("/setup")
-        public String greetingSubmit(@ModelAttribute SetupForm setup, Model model) {
-            System.out.println(setup.getJson());
+        public String setupSubmit(@ModelAttribute SetupForm setup, Model model){
+            String replaced = setup.getJson().replace("\r\n", " ");
+            setup.setJson(replaced);
             model.addAttribute("setup", setup);
+//            String json = setup.getJson();
+            String url = service + "createClient";
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Type", "application/json");
+            System.out.println(setup.getJson());
+            HttpEntity<String> entity = new HttpEntity<String>(setup.getJson(), headers);
+            restTemplate.postForEntity(url, entity, String.class);
+//            restTemplate.postForObject(url, setup.getJson(), String.class);
             return "setup_success";
         }
 
